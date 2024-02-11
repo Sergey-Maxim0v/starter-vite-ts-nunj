bef207556f71dceb3fd2f7eb05202420ddc14a67
Sergey Maximov <dev9092336062@gmail.com>
2024-02-11 21:50:57 +0300

    eslint

--- src/scripts/vite.tsx
+++ src/scripts/vite.tsx
@@ -4,13 +4,13 @@ const Vite = () => {
     return;
   }
 
-  return (appElement.innerHTML = `
+  appElement.innerHTML = `
     <h1 style="font-size: 3rem; margin-bottom: 40px">Vite + TypeScript</h1>
     
     <div class="card">
         <button id="counter" type="button" class="button" style="padding: 4px 20px"></button>
     </div>
-`);
+`;
 };
 
 export default Vite();
