18a7be299d97e3c83b9ebb2c7968428e7c4fe379
Sergey Maximov <dev9092336062@gmail.com>
2024-02-12 01:09:24 +0300

    fix page links

--- src/scripts/pageLinks.ts
+++ src/scripts/pageLinks.ts
@@ -1,7 +1,7 @@
 const elementLinkHome = document.querySelector('[data-page-link-home]');
 const elementLinkVite = document.querySelector('[data-page-link-vite]');
 
-const baseUrl = window.location.href.replace('vite', '');
+const baseUrl = window.location.href.replace('/vite', '');
 
 elementLinkHome?.setAttribute('href', `${baseUrl}`);
-elementLinkVite?.setAttribute('href', `${baseUrl}vite`);
+elementLinkVite?.setAttribute('href', `${baseUrl}/vite`);
